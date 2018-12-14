59380c69d10eea043f18f512f4bf2a4b02860081
Shahanmiah <45834909+Shahanmiah@users.noreply.github.com>
2018-12-14 14:27:08 +0000

    update 3n+1 .py

--- Maths/3n+1.py
+++ Maths/3n+1.py
@@ -1,19 +1,19 @@
 def main():
-    def n31(a):# a = initial number
+    def n23(b):# a = initial number
         c = 0
-        l = [a]
-        while a != 1:
-            if a % 2 == 0:#if even divide it by 2
-                a = a // 2
-            elif a % 2 == 1:#if odd 3n+1
-                a = 3*a +1
+        l = [b]
+        while b != 1:
+            if b % 2 == 0:#if even divide it by 2
+                b = b // 2
+            elif b % 2 == 1:#if odd 3n+1
+                b = 3*b +1
             c += 1#counter
-            l += [a]
+            l += [b]
 
         return l , c
-    print(n31(43))
-    print(n31(98)[0][-1])# = a
-    print("It took {0} steps.".format(n31(13)[1]))#optional finish
+    print(n23(43))
+    print(n23(98)[0][-1])# = a
+    print("It took {0} steps.".format(n23(13)[1]))#optional finish
 
 if __name__ == '__main__':
     main()
